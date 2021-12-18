cc9b84660acf1959e32c3f6a76dab5544f3e9ebd
fonfon08 <89083178+fonfon08@users.noreply.github.com>
2021-12-18 17:34:29 -0300

    Add files via upload

---
+++ quiz game example/README.txt
@@ -0,0 +1,3 @@
+to play the game go to where is written "quiz game example" and write "cmd" and enjoy (:
+
+to see the code and edit it simply right click it and them click on "edit" (:
---
+++ quiz game example/quizgame.py
@@ -0,0 +1,45 @@
+print("welcome to my quiz game")
+playing = input("do you want to play? ")
+
+if playing != ("yes"):
+	quit()
+
+print("ok lets play!")
+
+answer = input("what does LOL stands for? ")
+if answer == "laughing out loud":
+	print("Correct!")
+else:
+	print("incorrect!")
+
+answer = input("am i behind you?(: ")
+if answer == "yes":
+	print("Correct!")
+else:
+	answer = input("you sure? ") 
+	if answer == "yes":
+		print("you are not(:")	
+	else:
+		print("hehehe")
+
+
+answer = input("am i still here? ")
+if answer == "yes":
+	print("Correct(:")
+else:	
+	print("incorrect(: ")
+
+answer = input("what do i look  like? ")
+if answer == "a spoiled brat":
+	print("o that hurts")
+else:	
+	print("serius?")
+
+
+answer = input("never gonna give you up, never gonna let you down, fly arround and desert you ")
+if answer == "fuck you":
+	print("you deserved it lol")
+else:	
+	print("get rickrolled")
+
+	
